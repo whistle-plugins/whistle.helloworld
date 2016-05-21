8d1a87a0d204c917bf2b773d22b5081455f455fb
avwo <avwu@qq.com>
2016-05-21 18:37:08 +0800

    feat: Add rulesServer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var initPluginServer = require('./lib/plugin-server');
 var initUIServer = require('./lib/ui-server');
+var initRulesServer = require('./lib/rules-server');
 
 module.exports = function(server, options) {
 	initPluginServer(server, options);
@@ -7,4 +8,8 @@ module.exports = function(server, options) {
 
 module.exports.uiServer = function(server, options) {
 	initUIServer(server, options);
+};
+
+module.exports.rulesServer = function(server, options) {
+	initRulesServer(server, options);
 };
