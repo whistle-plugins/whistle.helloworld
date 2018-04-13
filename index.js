55a527e5187148edf0c9b5f7467288ced5bb6d31
avwo <avwu@qq.com>
2018-04-14 02:36:54 +0800

    init

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+/**
+ * 初始化钩子，whistle v1.9.6开始支持
+ */
+exports.initial = require('./lib/options');
+
+/**
+ * 统计服务
+ */
+//
 exports.statsServer = require('./lib/statsServer');
+exports.resStatsServer = require('./lib/resStatsServer');
+
+/**
+ *
+ */
 exports.rulesServer = require('./lib/rulesServer');
 exports.server = require('./lib/server');
 exports.resRulesServer = require('./lib/resRulesServer');
-exports.resStatsServer = require('./lib/resStatsServer');
 
+/**
+ *
+ */
 exports.tunnelRulesServer = require('./lib/tunnelRulesServer');
-// exports.tunnelServer =
+exports.tunnelServer = require('./lib/tunnelServer');
